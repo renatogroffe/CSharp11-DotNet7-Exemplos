4f469c98928d2ae35a60bc41b3187117c42e93a0
Renato Groffe <renato.groffe@yahoo.com.br>
2023-01-30 21:53:34 -0300

    Required Members
    Required Members

---
+++ ExemploRequiredMembers/Models/Funcionario.cs
@@ -0,0 +1,9 @@
+﻿namespace ExemploRequiredMembers.Models;
+
+public class Funcionario
+{
+    public required string Codigo { get; set; }
+    public required string Nome { get; set; }
+    public required double Salario { get; set; }
+    public string? Observacao { get; set; }
+}
---
+++ ExemploRequiredMembers/Program.cs
@@ -0,0 +1,20 @@
+﻿using ExemploRequiredMembers.Models;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com C# 11 | Required Members *****");
+Console.WriteLine($"Versão do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var func0001 = new Funcionario()
+{
+    Codigo = "0001",
+    Nome = "Joao da Silva",
+    Salario = 10000
+};
+Console.WriteLine($"Dados do Funcionario: {JsonSerializer.Serialize(func0001)}");
+
+func0001.Salario = 11000;
+func0001.Observacao = "Aumento de 10%";
+Console.WriteLine($"Dados do Funcionario apos reajuste: {JsonSerializer.Serialize(func0001)}");
