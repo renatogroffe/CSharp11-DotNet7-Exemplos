cf94bd05e32ae48c8fca68487843462bf0351e79
Renato Groffe <renato.groffe@yahoo.com.br>
2023-01-30 23:00:11 -0300

    Utf-8
    Utf-8

---
+++ ExemploUtf8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com C# 11 | UTF-8 String Literals *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var parte1 = "Exemplo utilizando Utf-8 no C# 11 "u8;
+ReadOnlySpan<byte> parte2 = "| Segunda parte do conteúdo "u8;
+
+using var fs = new FileStream($"exemplo-utf8-{DateTime.Now:MMdd-HHmmss}.txt",
+    FileMode.CreateNew);
+fs.Write(parte1);
+fs.Write(parte2);
+fs.Close();
+Console.WriteLine($"Arquivo '{fs.Name}' gerado com sucesso!");
