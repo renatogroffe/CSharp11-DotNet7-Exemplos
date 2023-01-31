f130fca37393db3744909083148e4065e450bb19
Renato Groffe <renato.groffe@yahoo.com.br>
2023-01-30 22:17:16 -0300

    List Patterns
    List Patterns

---
+++ ExemploListPatterns/Calendario.cs
@@ -0,0 +1,25 @@
+﻿namespace ExemploListPatterns;
+
+public static class Calendario
+{
+    public static string ObterEstacao(int[] meses)
+    => meses switch
+    {
+        [12, 1, 2, 3] => "Verão",
+        [3, .., 6] => "Outono",
+        [6, .., 9] => "Inverno",
+        [9, .., 12] => "Primavera",
+        [_] => "Mês qualquer",
+        [1, ..] => "Janeiro/Verão em diante",
+        [..] => "Uma sequência de meses"
+    };
+
+    public static string AnalisarSequenciaMeses(int[] meses)
+    => meses switch
+    {
+        [_, _] => $"2 Meses: {String.Join(", ", meses)}",
+        [1, .. var meses01, _] => $"Meses intermediários após Janeiro: {String.Join(", ", meses01)}",
+        [6, .. var meses02, _] => $"Meses intermediários após Junho: {String.Join(", ", meses02)}",
+        [.. var todosMeses] => $"Todos os meses: {String.Join(", ", todosMeses)}"
+    };
+}
---
+++ ExemploListPatterns/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+using ExemploListPatterns;
+
+Console.WriteLine("***** Testes com C# 11 | List Patterns *****");
+Console.WriteLine($"Versão do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var conjuntoMeses = new List<int[]>();
+conjuntoMeses.Add(new int[] { 3, 5, 6 });
+conjuntoMeses.Add(new int[] { 1 });
+conjuntoMeses.Add(new int[] { 6, 9 });
+conjuntoMeses.Add(new int[] { 1, 2, 3, 4, 5, 6, 7 });
+conjuntoMeses.Add(new int[] { 2, 4, 6, 8, 10 });
+conjuntoMeses.Add(new int[] { 12, 1, 2, 3 });
+conjuntoMeses.Add(new int[] { 6, 7, 8, 9 });
+conjuntoMeses.Add(new int[] { 9, 10, 12 });
+
+foreach (var meses in conjuntoMeses)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Meses: [ {String.Join(", ", meses)} ]");
+    Console.WriteLine($"Estação do ano: {Calendario.ObterEstacao(meses)}");
+    Console.WriteLine($"{Calendario.AnalisarSequenciaMeses(meses)}");
+}
