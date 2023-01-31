ba7ab6a622dffea1115c6fd48c55e1b6ca9d169b
Renato Groffe <renato.groffe@yahoo.com.br>
2023-01-31 07:30:45 -0300

    Generic Attributes
    Generic Attributes

---
+++ ExemploGenericAttributes/Classes.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+using System.Text.Json;
+
+namespace ExemploGenericAttributes.Models;
+
+[Document<JsonDocument>]
+public class TransacaoDebito
+{
+}
+
+[Document<JsonDocument>]
+public class TransacaoPix
+{
+}
+
+[Document<XDocument>]
+public class NotaFiscalEletronica
+{
+}
+
+[Document<XDocument>]
+public class NotaFiscalServicos
+{
+}
