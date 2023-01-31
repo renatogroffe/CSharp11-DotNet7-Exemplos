9b51c343cb7aa5653bf13e758402e90d4e8afaef
Renato Groffe <renato.groffe@yahoo.com.br>
2023-01-30 22:03:18 -0300

    String Interpolation
    String Interpolation

---
+++ ExemploStringInterpolation/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com C# 11 *****");
+Console.WriteLine($"Versão do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment
+    .MachineName} - Kernel: {Environment
+    .OSVersion
+        .VersionString}");
