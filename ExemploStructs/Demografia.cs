8df9d2e53348586fa12a189bf96e528ccc65acc0
Renato Groffe <renato.groffe@yahoo.com.br>
2023-01-30 22:10:57 -0300

    Auto-default structs
    Auto-default structs

---
+++ ExemploStructs/Demografia.cs
@@ -0,0 +1,13 @@
+﻿namespace ExemploStructs;
+
+public struct Demografia
+{
+    public string? Pais { get; init; }
+    public int Ano { get; set; }
+    public int Populacao { get; set; }
+    public bool MembroOCDE { get; set; }
+
+    public Demografia()
+    {
+    }
+}
---
+++ ExemploStructs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.Json;
+using ExemploStructs;
+
+Console.WriteLine("***** Testes com C# 11 | Auto-default structs *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var struct01 = new Demografia();
+Console.WriteLine($"{nameof(struct01)} = {JsonSerializer.Serialize(struct01)}");
+
+var struct02 = new Demografia()
+{
+    Pais = "Canada",
+    Ano = 2022,
+    Populacao = 38_929_902,
+    MembroOCDE = true
+};
+Console.WriteLine($"{nameof(struct02)} = {JsonSerializer.Serialize(struct02)}");
